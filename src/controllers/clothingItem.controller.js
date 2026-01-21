1be5d52f8771dec1e75c670a103253ff9bbe31cf
velourcodes <mohammadaffansiddiqi98@gmail.com>
2026-01-21 13:33:43 +0530

    Render error fixed

--- src/controllers/clothingItem.controller.js
+++ src/controllers/clothingItem.controller.js
@@ -23,9 +23,6 @@ const analyzeClothingImage = asyncHandler(async (req, res) => {
         console.error("AI Analysis Failed:", error);
         throw new ApiError(500, "AI analysis failed");
     }
-    finally {
-        fs.unlinkSync(req.file.path);
-    }
 });
 
 const addClothingItem = asyncHandler(async (req, res) => {
